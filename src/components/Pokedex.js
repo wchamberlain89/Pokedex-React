d6eeee34100e533f682207817f76c7be2f8713ed
wc <wchamberlain89@gmail.com>
2020-10-06 17:05:28 -0700

    adds home route redirect

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import Pokemon from './Pokemon'
 
 const Pokedex = () => {
@@ -8,6 +8,9 @@ const Pokedex = () => {
       <Switch>
         <Route path='/pokemon/:id'>
           <Pokemon />
+        </Route>
+        <Route path='/'>
+          <Redirect to='/pokemon/bulbasaur'/>
         </Route>
       </Switch>
     </>
